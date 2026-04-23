751a87b241e2c69d28238c1912f0e7a9aaafc752
Zaiter1 <miguelangelzaiter0@gmail.com>
2026-04-22 20:26:55 -0400

    Clean up comments in EquipmentDto properties
    Removed comments indicating the addition of nullable types.

--- ThePodcastProject.Application/Dtos/EquipmentDto.cs
+++ ThePodcastProject.Application/Dtos/EquipmentDto.cs
@@ -7,9 +7,9 @@ namespace ThePodcastProject.Application.Dtos
     public class EquipmentDto
     {
         public int Id { get; set; }
-        public  string ? Name { get; set; } //? agregado
-        public  string ? Type { get; set; } //? agregado
-        public  string ? Description { get; set; } //? agregado
+        public  string ? Name { get; set; } 
+        public  string ? Type { get; set; } 
+        public  string ? Description { get; set; } 
 
         public ICollection<ReservationDto> Reservations { get; set; } = new List<ReservationDto>();
 
